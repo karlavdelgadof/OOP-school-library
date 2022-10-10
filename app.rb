eedf08a1b55534f20a078cd52b26621e931d3b9d
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-10 14:47:12 -0500

    Refactor to use an array for options

--- app.rb
+++ app.rb
@@ -11,63 +11,73 @@ class App
     @rentals = []
   end
 
-  def app_options
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'.colorize(color: :red).underline
-    puts "\n\n"
-  end
+  @@app_options = [
+    '[0]  =>  List all books',
+    '[1]  =>  List all people',
+    '[2]  =>  List all students',
+    '[3]  =>  List all teachers',
+    '[4]  =>  Create a person',
+    '[5]  =>  Create a book',
+    '[6]  =>  Create a rental',
+    '[7]  =>  List all rentals for a given person id',
+    '[8]  =>  List all rentals for a given book',
+    '[9]  =>  Exit'    
+  ]
 
   def run
     user_response = 0
     puts "\n\nWelcome to School Library App!\n\n".colorize(color: :green).bold
-    while user_response != '7'
+
+    while user_response != '9'
       puts "Please choose an option by entering a number:\n\n".colorize(color: :magenta)
-      app_options
-      print 'Enter Option [number]: '.colorize(color: :white).bold
+      @@app_options.each do |choice|       
+        puts choice
+      end
+      print "\n\nEnter Option [number]: ".colorize(color: :white).bold
       user_response = gets.chomp
       puts "\n\n"
       check_selection(user_response)
-
     end
-    puts "Thank you for using this app!\n\n".colorize(color: :cyan).bold if user_response == '7'
+    puts "Thank you for using this app!\n\n".colorize(color: :cyan).bold if user_response == '9'
   end
 
   def check_selection(response)
     case response
-    when '1'
+    when '0'
       list_all_books
-    when '2'
+    when '1'
       list_all_people
+    when '2'
+      lits_all_students
     when '3'
-      create_person
+      list_all_teachers
     when '4'
-      create_book
+      create_person
     when '5'
-      create_rental
+      create_book
     when '6'
+      create_rental
+    when '7'
       list_all_rentals_person_id
+    when '8'
+      list_all_rentals_for_book
     end
   end
 
   def list_all_books
-    @books.each do |book|
+    @books.each do |book|     
       puts "Title: \"#{book.title}\" Author: #{book.author}"
     end
   end
 
   def list_all_people
-    @people.each do |person|
+    @people.each do |person|     
       puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   end
 
   def create_person
-    print 'Do you want to create a student (1) or a teacher (2)?[Input the number]:'
+    print 'Do you want to create a student (1) or a teacher (2)?[Input the number]: '
     person_choice = gets.chomp
     if person_choice != '1' && person_choice != '2'
       puts 'Invalid option'
@@ -123,7 +133,7 @@ class App
       print 'Date: '
       date = gets.chomp
       puts 'Select a person from the following list by number (not id)'
-      @people.each do |person|
+      @people.each do |person|       
         puts "#{@people.find_index(person)} - #{person.name}"
       end
       selected_person = gets.to_i
@@ -139,7 +149,7 @@ class App
     list_all_people
     print 'ID of person: '
     person_id = gets.to_i
-    puts(@rentals.map do |rental|
+    puts(@rentals.map do |rental|     
       if rental.person.id == person_id
         "Rentals:\nDate: #{rental.date}  Book: #{rental.book.title}"
       else
@@ -148,6 +158,3 @@ class App
     end)
   end
 end
-
-p String.colors
-p String.modes
