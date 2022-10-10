955382ea60b7bc62efd15343391ae898c2936543
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-10 16:36:50 -0500

    Create new method to list rentals by book

--- book.rb
+++ book.rb
@@ -10,4 +10,18 @@ class Book
   def create_rental(person, date)
     Rental.new(date, self, person)
   end
+
+  def self.list_all_rentals_for_book(books, rentals)
+    self.list_all_books(books)
+    print 'Enter Book Title: '
+    book_title = gets.chomp
+    puts(rentals.map do |rental|     
+      if rental.book.title == book_title
+        "Rentals:\nDate: #{rental.date}  Rented by: #{rental.person.name} (#{rental.person.id}"
+      else
+        'No rentals found for this book'
+      end
+    end)
+  end
+
 end
