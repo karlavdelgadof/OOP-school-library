266641cc17fe4ef12cd5e889ed52b102873c002e
karlavdelgadof <karlavdelgadof@gmail.com>
2022-10-10 19:38:22 -0500

    Add list and create methods to student/teacher

--- teacher.rb
+++ teacher.rb
@@ -6,6 +6,22 @@ class Teacher < Person
     @specialization = specialization
   end
 
+  def self.create_teacher(age, name)
+    print 'specialization: '
+    specialization = gets.chomp
+    new(age, name, true, specialization)
+  end
+
+  def self.list_all_teachers(people)
+    people.each do |person|
+      if person.class.to_s == 'Student'
+        'No teachers found, choose a different option to register a new teacher'
+      else
+        puts "Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+    end
+  end
+
   def can_use_services?
     true
   end
